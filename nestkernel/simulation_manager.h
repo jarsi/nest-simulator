6a2f6c0e88fb423ae7a4f53f31cd289cce6421cd
jarsi <jari.pronold@gmail.com>
2019-06-10 08:56:38 +0200

    Include Timers, activate with -DTIMER=1 or -DTIMER=ON

--- nestkernel/simulation_manager.h
+++ nestkernel/simulation_manager.h
@@ -28,6 +28,7 @@
 
 // Includes from libnestutil:
 #include "manager_interface.h"
+#include "stopwatch.h"
 
 // Includes from nestkernel:
 #include "nest_time.h"
@@ -192,6 +193,8 @@ private:
                                    //!< relaxation
   size_t wfr_interpolation_order_; //!< interpolation order for waveform
                                    //!< relaxation method
+
+  Stopwatch sw_simulate;
 };
 
 inline Time const&
